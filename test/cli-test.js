8484ceec6fc9800be0c44b4ed2fc091d2b03c4e6
Sebastian Schürmann <sebs@2xs.org>
2016-09-28 22:18:23 +0200

    (feature): jshint on build

--- test/cli-test.js
+++ test/cli-test.js
@@ -2,16 +2,14 @@
 const assert = require('chai').assert;
 var exec = require('child-process-promise').exec;
 
-var commands =
- ['./bin/capcoin -h',
-'./bin/capcoin coins -h',
-'./bin/capcoin front -h',
-'./bin/capcoin global -h',
-'./bin/capcoin history -h',
-'./bin/capcoin map -h',
-'./bin/capcoin page -h',
-
-
+var commands =[
+  './bin/capcoin -h',
+  './bin/capcoin coins -h',
+  './bin/capcoin front -h',
+  './bin/capcoin global -h',
+  './bin/capcoin history -h',
+  './bin/capcoin map -h',
+  './bin/capcoin page -h',
 ];
 
 describe('capcoin', function() {
@@ -19,19 +17,15 @@ describe('capcoin', function() {
   it('executes help', function(done) {
 
     function execAndDone(command){
-      return exec(command).catch(done)
+      return exec(command).catch(done);
     }
 
     commands = commands.map(execAndDone);
     Promise
       .all(commands)
       .then(function (result) {
-          result.map(assert.ok)
+          result.map(assert.ok);
           done();
       }).catch(done);
   }).timeout(5000);
-
-
-
-
 });
